c0cc66438c4cc2ed9a6d7cae44fbcdd6335471da
g3rry9898 <hydrogenbond36@gmail.com>
2024-12-05 04:06:15 -0800

    l

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,20 @@
+def island_perimeter(grid):
+    perimeter = 0
+    rows = len(grid)
+    cols = len(grid[0])
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                # Check all four sides
+                if i == 0 or grid[i-1][j] == 0:  # Top
+                    perimeter += 1
+                if i == rows-1 or grid[i+1][j] == 0:  # Bottom
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:  # Left
+                    perimeter += 1
+                if j == cols-1 or grid[i][j+1] == 0:  # Right
+                    perimeter += 1
+
+    return perimeter
+
